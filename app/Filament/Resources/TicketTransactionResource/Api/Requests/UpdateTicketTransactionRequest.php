57bf0a6a1717a9a93a4cf0c88fef1a755a6cece1
RizafiDev <firmansyah.riza6@smk.belajar.id>
2025-04-20 21:12:58 +0700

    finish: payment method

--- app/Filament/Resources/TicketTransactionResource/Api/Requests/UpdateTicketTransactionRequest.php
+++ app/Filament/Resources/TicketTransactionResource/Api/Requests/UpdateTicketTransactionRequest.php
@@ -21,14 +21,22 @@ class UpdateTicketTransactionRequest extends FormRequest
      */
     public function rules(): array
     {
+        if ($this->isMethod('PUT') || $this->isMethod('PATCH')) {
+            return [
+                'status' => 'required|string',
+            ];
+        }
+    
+        // Default (misalnya untuk POST)
         return [
-			'order_id' => 'required|string',
-			'appuser_id' => 'required|integer',
-			'schedule_id' => 'required|integer',
-			'seats' => 'required',
-			'gross_amount' => 'required|integer',
-			'status' => 'required',
-			'snap_token' => 'nullable|string'
-		];
+            'order_id' => 'required|string',
+            'appuser_id' => 'required|integer',
+            'schedule_id' => 'required|integer',
+            'seats' => 'required',
+            'gross_amount' => 'required|integer',
+            'status' => 'required',
+            'snap_token' => 'nullable|string'
+        ];
     }
+    
 }
